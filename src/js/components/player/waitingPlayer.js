73c8615f3d9401cb9492db3092fc7154c12fdc61
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2018-01-07 16:45:35 +0000

    WIP

--- src/js/components/player/waitingPlayer.js
+++ src/js/components/player/waitingPlayer.js
@@ -9,7 +9,9 @@ export default class Player extends React.Component {
   constructor(props) {
     super(props);
     this.db = this.props.database.child(this.props.index);
+    this.handleChangeEvent = this.handleChangeEvent.bind(this);
     this.state = {
+      data: "",
       slices: [
         {
           color:"#F04043",
@@ -40,39 +42,35 @@ export default class Player extends React.Component {
           value: 0.5
         },
       ],
-      userNames: [],
-      audioContext: this.props.audioContext,
-      index: this.props.index,
-      image: new Image(),
-      timer: Math.round(new Date().getTime() / 1000),
-      loop: 1,
-      mute: 0,
-      time: 0,
-      autoPlay: true
     };
   }
 
+  returnCurrentDate () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
 
-  addSlice(color, value) {
-    addSliceToDatabase.bind(this)(color, value);
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
+    return dd+'/'+mm+'/'+yyyy;
   }
 
-  /// ON/OFF SWITCH TRIGERED BY PLAY BUTTON PLUS ANIMATION
-  onOffSwitch() {
-    playerSwitch.bind(this)()
-    browserHistory.push('/details1')
+  handleChangeEvent(event) {
+    this.setState({data: event.target.value});
   }
+
 
   render() {
     return (
-
           <div className="player">
-
             <div className="wrapper"
                  id={"container" + this.state.index}>
             <PieChart
-              addSlice={this.addSlice.bind(this)}
-              storage={this.props.storage}
               db={this.db}
               slices={this.state.slices}
               loop={this.state.loop}
@@ -84,27 +82,48 @@ export default class Player extends React.Component {
 
             <AlbumCover index={this.state.index} />
 
-            <p className="waiting-title">
+            <div className="waiting-title">
               <p className="waiting-title-welcome">WELCOME</p>
               <p className="waiting-title-main">TO A NEW ECONOMY OF MUSIC PUBLICATION</p>
-            </p>
+            </div>
             <div className="waiting-title-line">
               <div className="waiting-title-line-dot"></div>
               <div className="waiting-title-line-dot right"></div>
             </div>
-            <p className="waiting-brand-name">
+            <div className="waiting-brand-name">
               <div className="  brand-name">MURMUR.fm</div>
-            </p>
+            </div>
             <div className="waiting-brand-name-line">
               <div className="waiting-brand-name-dot"></div>
               <div className="waiting-brand-name-dot right"></div>
               <div className="waiting-brand-name-dot current-time">
-                <p className="red-text">05/01/2018</p>
+                <p className="red-text">{this.returnCurrentDate()}</p>
               </div>
             </div>
-            <p className="waiting-sign-up">
-              <span>SIGN UP</span>
-            </p>
+            <div className="waiting-sign-up">
+              <div id="mc_embed_signup_scroll">
+              <form action="https://murmur.us17.list-manage.com/subscribe/post?u=8a759dc0e2dd1f954c877c33e&amp;id=22db0bc5df"
+                    method="post"
+                    defaultValue={this.state.data}
+                    placeholder= "SIGN UP HERE"
+                    value={this.state.data}
+                    onChange={this.handleChangeEvent}
+                    id="mc-embedded-subscribe-form"
+                    name="mc-embedded-subscribe-form"
+                    class="validate form-subscribe"
+                    target="_blank"
+                    noValidate>
+                <input type="email" value=""
+                placeholder= "SIGN UP HERE"
+                value={this.state.data}
+                onChange={this.handleChangeEvent}name="EMAIL" class="required email" id="mce-EMAIL"></input>
+
+                <div class="clear">
+                  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button subscribe-btn"></input>
+                </div>
+              </form>
+              </div>
+            </div>
             <div className="waiting-sign-up-line">
               <div className="waiting-sign-up-line-dot"></div>
                 <div className="waiting-sign-up-line-dot right"></div>
@@ -112,7 +131,7 @@ export default class Player extends React.Component {
               <div className="waiting-vertical-line right"></div>
             </div>
             <div className="waiting-date">
-              <span>April 2018</span>
+              <span>Streaming April 2018</span>
             </div>
           </div>
         </div>
