faeec8c4272dd5669104af09100fb99f1e38f4a9
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2018-01-18 21:02:44 +0000

    WIP

--- src/js/components/player/waitingPlayer.js
+++ src/js/components/player/waitingPlayer.js
@@ -10,11 +10,20 @@ export default class Player extends React.Component {
                      src={"./public/content/images/logo1.svg"} >
                 </img>
               </div>
-              <div className="waiting-top-grid"></div>
-              <div className="waiting-bottom-grid">
-                <div>
-                  <p>murmur.fm</p>
-                </div>
+              <div className="waiting-top-grid">
+                <p className="waiting-header">Publishing</p>
+                <p className="waiting-header sub">together</p>
+              </div>
+              <div className="row">
+                <div className="col s2"></div>
+                <div className="col s1">FM</div>
+                <div className="col s1">TW</div>
+                <div className="col s1">MD</div>
+                <div className="col s2"></div>
+                <div className="col s2 brand">MURMUR.FM</div>
+              </div>
+              <div className="waiting-bottom-grid ">
+                STREAMING APRIL 2018
               </div>
               <div className="waiting-title-line"></div>
               <div className="waiting-sign-up-line">
